8ff1be43dc6aff23c2a2c08a3eeafa9aafa90db8
Jorge Caballero <joalbes@gmail.com>
2023-03-10 13:56:34 -0500

    10 marzo clase tarde

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -5,31 +5,32 @@ const arregloPalabras = ["hola","coex","developer","javascript","html"];
 const imagen = document.getElementById('imagen_ahorcado');
 const palabraAvalidar = document.getElementById('contendor_palabra_a_validar');
 const contedorHistorico = document.getElementById('contedor-historico');
-const btnBuscarPalabra = document.getElementById('');
+const btnBuscarPalabra = document.getElementById('btn_buscar_palabra');
 const btnJuagar = document.getElementById('btn_jugar');
 const btnReset = document.getElementById('btn_reset');
 //Otras variables
 let palabra;
-let numeroPosicionesPalabra;
+let numeroPosicionesPalabra = '';
 
 
 /******************************************** OBTENIENDO VALORES ***********************************************/
-const entradaLetras = document.querySelector('#contedor-historico').value;
+const entradaLetras = document.querySelector('#entrada_letra').value;
 /******************************************* ADICIONANDO EVENTOS ***********************************************/
 btnBuscarPalabra.addEventListener('click',fBuscarPalabra);
-btnJuagar.addEventListener('click',fJugar);
+btnJuagar.addEventListener('click',fEnter);
 btnReset.addEventListener('click',fReset);
 /************************************************ FUNCIONES ****************************************************/
 function fBuscarPalabra(){
-    palabra = arregloPalabras[Math.floor(Math.random() * arregloPalabras.length)];
-    
-
-
-
-
+     palabra = arregloPalabras[Math.floor(Math.random() * arregloPalabras.length)];
+      for (let index = 0; index < palabra.length; index++) {
+        numeroPosicionesPalabra +='__   ';       
+        }
+        palabraAvalidar.innerHTML = `<p>${numeroPosicionesPalabra}</p>`
+                                 
 }
 
-function fJugar(){
+function fEnter(){
+    contedorHistorico.innerHTML = `<p>${entradaLetras}</p>`
 
 }
 
