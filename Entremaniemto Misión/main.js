568e813ee8ccebf4f9ab245ea8d15c0746a1f5ec
Jorge Caballero <joalbes@gmail.com>
2023-03-28 13:49:02 -0500

    hora 1:48

--- Entremaniemto Misión/main.js
+++ Entremaniemto Misión/main.js
@@ -5,6 +5,9 @@ const contenedorCartas = document.getElementById("contenedorCard");
 //const carta = document.getElementsByTagName("id");
 
 
+function setLocal(event) {
+    localStorage.setItem('id', event.target.id)
+}
 
 const main = async ()=> {
     const responses = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -15,25 +18,16 @@ const main = async ()=> {
         contenedorCartas.innerHTML +=                    
         `<!-- card -->
             <div id="${data[i].id}" class=" w-[15rem] bg-orange-400 rounded-lg p-[1rem]">
-                <h1 class="font-bold text-sm  pb-[1rem]">${data[i].title}</h1>
-                <p class="text-xs">${data[i].body}</p>
-            </div>`  
-         arrayCarta[i] = 'carta'.concat(`${i}`) 
-         arrayIds[i] = data[i].id        
+                <h1 id="${data[i].id}" class="font-bold text-sm  pb-[1rem]">${data[i].title}</h1>
+                <p id="${data[i].id}" class="text-xs">${data[i].body}</p>
+            </div>`           
                 
     } 
 
-    
+    for (let index = 0; index < 20; index++) {
+      document.getElementById(`${data[index].id}`).addEventListener('click',setLocal)
+        
+    }   
 }
-
-function crearReferencias() {
-     
-    const carta0 = document.querySelector(`id0`);
-    console.log(carta0);
-
-    
-}
-
 
 main();
-crearReferencias();
